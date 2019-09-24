9ff8202b0ec0e66d621b6ec00918a3dcef7c964d
hidri <agthidri2400@gmail.com>
2019-09-24 17:33:57 +0300

    this commit will ad the form in auth

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,2 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField,TextAreaField
+from wtforms.validators import Required,Email,EqualTo,ValidationError
+from ..models import User
+
+class RegitrationForm(FlaskForm):
+    username = StringField('Enter Email Address', validators=[Required(),Email()])
+    email = StringField('Enter your username',validators=[Required(),Email()])
+    password = PasswordField('Enter a password: ',validators=[Required(),EqualTo('password_confirm',message='passwords must match')])
+    Submit = SubmitField('sign up')
+    
+    def validate_email(self,data_field):
+        if User.query.filter_by(email = data_field.data).first():
+            
+    
+    
