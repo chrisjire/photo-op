5055821fa64c9821684a837a0dce972a30a11999
hidri <agthidri2400@gmail.com>
2019-09-25 09:43:13 +0300

    this commit will add the files to the  views

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,47 +1,52 @@
-from .import auth
-from flask import render_template,request,redirect,url_for,flash
-from app.models import User
-from flask_login import login_user,logout_user
 
-@auth.route('/login',methods=['POST','GET'])
+
+
+from . import auth
+from flask import render_template,redirect,url_for,flash,request
+from .. import db,mail
+from .forms import RegistrationForm,LoginForm
+from ..models import User,Blog
+from flask_login import login_user,logout_user,login_required
+from flask_mail import Message
+
+
+@auth.route("/login", methods=['GET','POST'])
 def login():
-    if request.method == 'POST':
-        form = request.form
-        username = form.get('username')
-        password = form.get('password')
-        user = User.query.filter_by(username = username).first()
-        if user != None:
-            if user.check_password(password) != False:
-                flash("Succesfully logged in",'success')
-                login_user(user)
-                return redirect(url_for('main.index'))
-            
-            else:
-                flash("Invalid Password","login")
-                return render_template('auth/login.html',username=username)
-        else:
-                flash("Invalid Username","login")
-                return render_template('auth/login.html')
-    return render_template('auth/login.html')
 
-@auth.route('/signup',methods=['POST','GET'])
-def signup():
-    if request.method == "POST":
-        form = request.form
-        username = form.get('username')
-        email = form.get('email')
-        password =form.get('password')
-        confirm_password = form.get('confirm_password')
-        if password != confirm_password:
-            flash("passwords dont match","password")
-            return render_template('auth/signup.html',email=email, username = username)
-        user = User.query.filter_by(username = username).first()
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+      
+        user = User.query.filter_by(email =login_form.email.data).first()
+        if user == None:
+            flash('User name or password does not exist')
+            return render_template('auth/login.html',title='Login', form=form , posts=new_post)
+        
+        if user.password == login_form.password.data: 
+            login_user(user)
+            flash('You  have been logged in!' ,'success')
+            return redirect(url_for('main.index'))
         else:
-            flash("Username is already taken","username")
-            return render_template('auth/signup.html',email = email, password = password, confirm_password = confirm_password)
-    return render_template('auth/signup.html')
+            flash('Login Unsuccessful.Please check username and Password!','danger')
+            return redirect(request.args.get('next') or url_for('main.index'))
+    return render_template('login.html',loginF = login_form)
+
+
+
+
+
+
+
+@auth.route('/signup',methods = ["GET","POST"])
+def signup():
+  form = RegistrationForm()
+  if form.validate_on_submit():
+    user = User(email = form.email.data,username = form.username.data,password = form.password.data)
+    user.save()
+    return redirect(url_for('auth.login'))
+  return render_template('signup.html',regi_form = form)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    logout_user()
-    return redirect(url_for('main.index'))
+  logout_user()
+  return redirect(url_for('main.index'))
