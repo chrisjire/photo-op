55b11c7d0c51c64d9ecdffc0aec47ce8e49d84d2
chris <christianwilliamsjire@gmail.com>
2019-09-26 08:43:15 +0300

    this commit willchange the layout of the home page

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
+from . import db 
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     
