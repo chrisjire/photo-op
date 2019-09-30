983442fe50490eeb4cb96a7e049877354949bd1a
hidri <agthidri2400@gmail.com>
2019-09-30 08:05:41 +0300

    commit

--- app/models.py
+++ app/models.py
@@ -50,7 +50,7 @@ class Role(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
-    # users = db.relationship('User', backref='role', lazy="dynamic")
+   
 
 
 
